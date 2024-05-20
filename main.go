db84649fc9107ef6d5416f7b84acb957ddf7b586
aneesazc <anees.azc@gmail.com>
2024-05-20 13:33:57 +0530

    custom handler

--- main.go
+++ main.go
@@ -11,7 +11,13 @@ func main() {
 
 	mux := http.NewServeMux()
 	// Serve all files from the root directory
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	// mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte("OK"))
+	})
 
 	srv := &http.Server{
 		Addr:    ":" + port,
